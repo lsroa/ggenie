ca1cd4e759abe1135b47d10f9b32542abf607d44
Luis Roa <lsroa@macbook.local>
2025-05-05 12:54:55 +0200

    Fix python tank instance coordinates

--- bindings/main.py
+++ bindings/main.py
@@ -3,8 +3,8 @@ from gg import Game, Sprite, Transform, Animation
 
 def setup(g: Game) -> None:
     print("Hello from python")
-    s = Sprite(id="tank", rect=(10, 10, 10, 10), scale=(3, 3))
-    t = Transform(x=10.0, y=10.0)
+    s = Sprite(id="tank", rect=(0, 16, 16, 16), scale=(3, 3))
+    t = Transform(x=0.0, y=0.0)
     a = Animation(2, 4)
 
     tank = g.create_entity()
--- src/Game/game.h
+++ src/Game/game.h
@@ -155,13 +155,6 @@ class Game {
         }
       }
 
-      /* auto tank_1 = registry->CreateEntity(); */
-      /* tank_1.AddComponent<Transform>(glm::vec2(0.0, 0.0)); */
-      /* tank_1.AddComponent<RigidBody>(glm::vec2(1.0, 0.0)); */
-      /* tank_1.AddComponent<Sprite>("tank", 0, 16, 16, 16, glm::vec2(SCALE)); */
-      /* tank_1.AddComponent<Animation>(2, 4); */
-      /* tank_1.AddComponent<BoxCollider>(16 * SCALE, 16 * SCALE); */
-
       auto tank_2 = registry->CreateEntity();
       tank_2.AddComponent<Transform>(glm::vec2(100, 0));
       /* tank_2.AddComponent<RigidBody>(glm::vec2(-1.0, 0.0)); */
